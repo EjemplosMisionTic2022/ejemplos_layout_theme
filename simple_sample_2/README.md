a7aa4b6a3ba744ee3c9ba9bf97f7fcf852d79995
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2021-11-12 11:04:23 -0500

    Add more images

--- simple_sample_2/README.md
+++ simple_sample_2/README.md
@@ -10,4 +10,10 @@ Implementación del ListViewBuilder
 
 <img src="listViewBuilder.png" width="300" />
 
+Implementación de un ListViewBuilder en el que se pueden agregar entradas (usando un Widget con estado)
+
+<img src="dynamicListView.png" width="300" />
+
+Por último, una implementación en la que las filas tienen una estructura más compleja 
+
 <img src="https://user-images.githubusercontent.com/28717626/136316275-99cb8da8-2ba6-4680-bd83-cf020ab6d468.gif" width="300" />
--- simple_sample_2/lib/main.dart
+++ simple_sample_2/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_sample_2/ui/widgets/dynamic_list.dart';
 import 'package:simple_sample_2/ui/widgets/simple_list.dart';
 
 import 'ui/widgets/list_builder.dart';
@@ -12,15 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'List on Flutter',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        //home: SimpleList(),
-        home: ListBuilder()
-        //home: DynamicList(),
-        //home: StylizedCard(),
-        );
+      title: 'List on Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //home: SimpleList(),
+      //home: ListBuilder()
+      home: DynamicList(),
+      //home: StylizedCard(),
+    );
   }
 }
